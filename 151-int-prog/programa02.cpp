8f292438fbab05d8a75ba18c787374adc8256b28
leanfj <leanfj@hotmail.com>
2020-02-12 18:47:04 -0300

    Declaração de variáveis

---
+++ 151-int-prog/programa02.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int numero1 = 10;
+    int numero2;
+    int numero3;
+
+    int numero4, numero5, numero6;
+
+    numero4 = 20;
+
+    char letra = 'a';
+
+    float media = 5.44;
+
+    return 0;
+}
