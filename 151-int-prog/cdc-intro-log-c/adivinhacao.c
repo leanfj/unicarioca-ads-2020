ff0df24537dad28f00dbcc868935e09b321a2598
leanfj <leanfj@hotmail.com>
2020-03-16 18:33:43 -0300

    Iniciando livro

---
+++ 151-int-prog/cdc-intro-log-c/adivinhacao.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("**********************************\n");
+    printf("*Bem Vindo ao Jogo da Adivinhação*\n");
+    printf("**********************************\n");
+
+    int numerosecreto;
+
+    numerosecreto = 42;
+
+    printf("%d \n", 42);
+
+    printf("O numero %d é secreto \n");
+}
