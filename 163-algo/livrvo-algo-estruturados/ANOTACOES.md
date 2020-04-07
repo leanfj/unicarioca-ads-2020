f0f0bfda12f7586884a7475b92d64fbe84250889
leanfj <leanfj@hotmail.com>
2020-04-06 23:25:11 -0300

    Modificação de exercicio

---
+++ 163-algo/livrvo-algo-estruturados/ANOTACOES.md
@@ -0,0 +1,21 @@
+# Capitulo 01
+
+- 1.1 Constantes
+     Valores fixos que não podem ser modificado durantes a execução de um programa.
+
+  - 1.1.1 Constantes numéricas
+    * Usando o sistema decimal para números com e sem partes fracionárias.
+  - 1.1.2 Constantes lógicas
+    * Podem ser somente `verdadeiro` e `falso`.
+  - 1.1.3 Constante literais
+    * Qualquer sequência de caractéres. Seram identificados pelo uso de aspas, duplas ou simples `"" , ''`
+
+  Exemplos:
+
+    `20` - numérica
+    `"Leandro"` - literais
+    `falso` - lógicas
+
+- 1.2 Variáveis
+
+  
--- 163-algo/quest4.c
+++ 163-algo/quest4.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 
-main () {
-	int a = 1, b = 3, c = 5;
+int main () {
+  int a = 1, b = 3, c = 5;
 
-	while((b != a) && (c < 20) ) {
-		if ( a > c ) {
-			c = c - 2;
-		} else {
-			c = c + 2;
-			if (a + b < c) {
-				a = b - a;
-				b = b + 2;
-			}
-		}
-	}
+  while((b != a) && (c < 20) ) {
+    if ( a > c ) {
+      c = c - 2;
+    } else {
+      c = c + 2;
+      if (a + b < c) {
+        a = b - a;
+        b = b + 2;
+      }
+    }
+  }
+  printf("Ola mundo \n");
+  printf("O Unico-valor final de A -> %d \n", a);
+  printf("O Unico-valor final de B -> %d \n", b);
+  printf("O Unico-valor final de C -> %d \n", c);
 
-	printf("O valor final de A -> %d \n", a);
-	printf("O valor final de B -> %d \n", b);
-	printf("O valor final de C -> %d \n", c);
+  return 0;
 }
-	
+
