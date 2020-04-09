dda768ed4ae4479d7a3990e65477d4e29c7208fe
leanfj <leanfj@hotmail.com>
2020-04-09 15:56:33 -0300

    Modificação de programade calculo de media para usar o FOR

--- 151-int-prog/tema-06/prog05.c
+++ 151-int-prog/tema-06/prog05.c
@@ -1,24 +1,35 @@
 #include <stdio.h>
 
-int main ( ) {
-  float nota01, nota02, media;
+int main() {
+  float nota01, nota02, media, media_turma = 0;
+  int contador, quant_alunos;
 
-  printf("Informe a primeira nota: \n");
-  scanf("%f", &nota01);
+  printf("Informa a quantidade de alunos: \n");
+  scanf("%d", &quant_alunos);
 
+  for (contador = 1; contador <= quant_alunos; contador = contador + 1) {
 
-  printf("Informe a segunda nota: \n");
-  scanf("%f", &nota02);
+    printf("Informe a primeira nota: \n");
+    scanf("%f", &nota01);
 
-  media = (nota01 + nota02) / 2;
+    printf("Informe a segunda nota: \n");
+    scanf("%f", &nota02);
 
-  printf("Média: %.1f \n", media);
+    media = (nota01 + nota02) / 2;
 
-  if (media >= 7.0) {
-    printf("Aprovado !!!");
-  } else {
-    printf("Reprovado!!!");
+    media_turma += media;
+
+    printf("Média: %.1f \n", media);
+
+    if (media >= 7.0) {
+      printf("Aprovado !!! \n");
+    } else {
+      printf("Reprovado!!! \n");
+    }
   }
+
+  printf("Total de alunos: %d \n", quant_alunos);
+  printf("Média da nota da turma: %.1f \n", media_turma / quant_alunos);
 
   return 0;
 }
