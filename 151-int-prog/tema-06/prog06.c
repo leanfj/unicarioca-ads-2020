cd06862eb22a0219d2f2141d6badc320b9bee0dc
leanfj <leanfj@hotmail.com>
2020-04-09 16:03:27 -0300

    Atividade 01 tabuada de 10

---
+++ 151-int-prog/tema-06/prog06.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main () {
+  printf("****Tabuada de 10***** \n");
+
+  int contador;
+
+  for (contador = 0; contador <= 10; contador++){
+
+    printf("%d X 10 = %d \n", contador, contador * 10);
+  }
+  return 0;
+}
