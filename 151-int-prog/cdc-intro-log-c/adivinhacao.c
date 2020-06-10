b1b5c700b644dc9fa8c175a221cf2867b5985d9f
leanfj <leanfj@hotmail.com>
2020-06-09 23:48:25 -0300

    Recebendo dados

--- 151-int-prog/cdc-intro-log-c/adivinhacao.c
+++ 151-int-prog/cdc-intro-log-c/adivinhacao.c
@@ -6,10 +6,18 @@ int main() {
   printf("**********************************\n");
 
   int numeroSecreto;
+  int chute;
 
   numeroSecreto = 42;
 
   printf("%d \n", numeroSecreto);
 
   printf("O %d é o numero secreto \n", numeroSecreto);
+
+  printf("De seu chute! \n");
+
+  scanf("%d", &chute);
+
+  printf("Seu chute é %d \n", chute);
+
 }
