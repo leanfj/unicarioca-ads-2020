d56cde185b71ef77096ad3d0bc382f0bfd961546
leanfj <leanfj@hotmail.com>
2020-06-21 18:11:46 -0300

    Operações com variáveis

--- 151-int-prog/cdc-intro-log-c/adivinhacao.c
+++ 151-int-prog/cdc-intro-log-c/adivinhacao.c
@@ -10,19 +10,20 @@ int main() {
   int tentativa;
   int ganhou = 0;
   int tentativas = 1;
+  int ponto = 1000;
 
   numeroSecreto = 42;
 
   while (!ganhou) {
-    printf("Qual é o seu %d, chute? \n", tentativas);
+    printf("Qual é o seu %d, chute? \n ", tentativas);
     scanf("%d", &tentativa);
 
     if (tentativa < 0) {
-      printf("Você não pode usar números negativos. \n");
+      printf("Você não pode usar números negativos. \n \n");
       continue;
     }
 
-    printf("Seu %d chute foi %d \n", tentativas, tentativa);
+    printf("Seu %d chute foi %d \n \n", tentativas, tentativa);
     int acertou = tentativa == numeroSecreto;
     int maior = tentativa > numeroSecreto;
     /*printf("Maior %d", maior);
@@ -30,7 +31,7 @@ int main() {
      * */
     if (acertou) {
       printf("Você acertou, parabêns \n");
-      printf("Você é um bom jogador! Tente novamente. \n");
+      printf("Você é um bom jogador!\n \n");
       ganhou = 1;
     } else if (maior) {
       printf("Sua tentativa foi maior que o numero secreto \n");
@@ -38,6 +39,11 @@ int main() {
       printf("Sua tentativa foi menor que o numero secreto \n");
     }
     tentativas++;
+    int ponto_perdidos = (tentativa - numeroSecreto) / 2;
+    ponto = ponto - ponto_perdidos;
+
   }
+  printf("Você fez %d pontos \n", ponto);
+  printf("Obrigado por jogar \n \n");
   printf("Fim de jogo \n");
 }
