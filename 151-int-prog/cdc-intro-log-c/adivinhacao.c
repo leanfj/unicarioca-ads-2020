5e75f50589f8fe7479abd312fe2cf71745ee6ac3
leanfj <leanfj@hotmail.com>
2020-06-24 10:37:32 -0300

    Adcionando nivel de dificuldade

--- 151-int-prog/cdc-intro-log-c/adivinhacao.c
+++ 151-int-prog/cdc-intro-log-c/adivinhacao.c
@@ -1,51 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define NUMERO_TENTATIVAS 3
 
 int main() {
   printf("**********************************\n");
   printf("*Bem Vindo ao Jogo da Adivinhação*\n");
   printf("**********************************\n");
 
+  int nivel;
+  int totaldetentativas;
   int numeroSecreto;
   int tentativa;
   int ganhou = 0;
-  int tentativas = 1;
   double ponto = 1000;
-  
   int segundos = time(0);
 
   srand(segundos);
-  
+
   int n1 = rand();
 
   numeroSecreto = n1 % 100;
 
-  while (!ganhou) {
-    printf("Qual é o seu %d, chute? \n ", tentativas);
+  printf("Qual o nível de dificuldade ? \n");
+  printf("(1)Fácil (2)Médio (3)Difícil \n \n");
+  printf("Escolha: ");
+
+  scanf("%d", &nivel);
+
+  if (nivel == 1) {
+    totaldetentativas = 20;
+  } else if (nivel == 2) {
+    totaldetentativas = 15;
+  } else {
+    totaldetentativas = 6;
+  }
+
+  for (int i = 1; i <= totaldetentativas; i++) {
+    printf("Tentativa %d de %d \n", i, totaldetentativas);
+
     scanf("%d", &tentativa);
 
     if (tentativa < 0) {
       printf("Você não pode usar números negativos. \n \n");
+      i--;
       continue;
     }
 
-    printf("Seu %d chute foi %d \n", tentativas, tentativa);
     int acertou = tentativa == numeroSecreto;
     int maior = tentativa > numeroSecreto;
-    /*printf("Maior %d", maior);
-     *Uso da variável fora do escopo, não ai funcionar!!!
-     * */
+
     if (acertou) {
       printf("Você acertou, parabêns \n");
-      ganhou = 1;
+      break;
     } else if (maior) {
       printf("Sua tentativa foi maior que o numero secreto \n");
     } else {
       printf("Sua tentativa foi menor que o numero secreto \n");
     }
-    tentativas++;
+
     double ponto_perdidos = (double)abs(tentativa - numeroSecreto) / (double)2;
     ponto = ponto - ponto_perdidos;
   }
