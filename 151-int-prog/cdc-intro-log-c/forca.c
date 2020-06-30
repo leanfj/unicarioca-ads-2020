3ab1d4740aeeaa8934294a8b3c1465e57d5199b6
leanfj <leanfj@hotmail.com>
2020-06-30 20:24:40 -0300

    Verificando com loops cada letra da palavra

--- 151-int-prog/cdc-intro-log-c/forca.c
+++ 151-int-prog/cdc-intro-log-c/forca.c
@@ -4,7 +4,8 @@
 int main() {
   char palavraSecreta[20];
   int acertou = 0, enforcado = 0;
-
+  char chutes[26];
+  int tentativas = 0;
   sprintf(palavraSecreta, "MELANCIA");
 
   do {
@@ -12,14 +13,29 @@ int main() {
 
     printf("Qual letra? \n");
     scanf(" %c", &chute);
+    chutes[tentativas] = chute;
+    tentativas++;
 
     /*Verifica em cada posicao do array se o chute é igual a letra da posição e
      * imprime na tela*/
     for (int i = 0; i < strlen(palavraSecreta); i++) {
-      if (palavraSecreta[i] == chute) {
-        printf("A posição %d tem essa letra \n", i + 1);
+      int achou = 0;
+
+      for (int j = 0; j < tentativas; j++) {
+        if (chutes[j] == palavraSecreta[i]) {
+          achou = 1;
+          break;
+        }
+
       }
+        if (achou) {
+          printf("%c ", palavraSecreta[i]);
+        } else {
+          printf("_ ");
+        }
     }
+
+    printf("\n");
   } while (!acertou && !enforcado);
 
   return 0;
