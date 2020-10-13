289c6d98543d9f483b3aea64dfd618edca0cbacf
Leandro Ferreira <leanfj@hotmail.com>
2020-10-13 18:36:00 -0300

    Atividade da AV1 e remoção de imagens

--- 163-algo/av1/quest1.c
+++ 163-algo/av1/quest1.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 
-  int x, y, z;
- int f (int *a, int *b, int c) {
-    while (*b <= c) {
-      *a = *b + c;
-      y = *b + 1;
-    }
-    int retorno = *a+*b+c;
-    return retorno;
+int x, y, z;
+int f (int *a, int *b, int c) {
+  while (*b <= c) {
+    *a = *b + c;
+    y = *b + 1;
   }
+  int retorno = *a+*b+c;
+  return retorno;
+}
+
 int main()
 {
   x = 10;
   y = 2;
   z = 3;
 
+  printf("%d, %d, %d \n", x, y, z);
   int valorF = f(&x, &y, z);
 
   printf("%d, %d, %d \n", x, y, z);
